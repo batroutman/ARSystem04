4f28c5351388a97b29bae5ea73d56236f834b1f4
batroutman <pencilboy20@sbcglobal.net>
2021-01-26 21:56:10 -0500

    Added reprojection evaluation to map point triangulation and reworked locking on fullBA.

--- src/main/java/ARSystem04/MapOptimizer.java
+++ src/main/java/ARSystem04/MapOptimizer.java
@@ -3,11 +3,20 @@ package ARSystem04;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.ddogleg.optimization.lm.ConfigLevenbergMarquardt;
+
+import boofcv.abst.geo.bundle.BundleAdjustment;
+import boofcv.abst.geo.bundle.ScaleSceneStructure;
+import boofcv.abst.geo.bundle.SceneObservations;
 import boofcv.abst.geo.bundle.SceneStructureMetric;
+import boofcv.alg.geo.bundle.cameras.BundlePinhole;
+import boofcv.factory.geo.ConfigBundleAdjustment;
+import boofcv.factory.geo.FactoryMultiView;
 import georegression.geometry.ConvertRotation3D_F64;
 import georegression.struct.point.Vector3D_F64;
 import georegression.struct.se.Se3_F64;
 import georegression.struct.so.Quaternion_F64;
+import runtimevars.CameraIntrinsics;
 import toolbox.Utils;
 import types.Point3D;
 import types.Pose;
@@ -29,13 +38,21 @@ public class MapOptimizer {
 	}
 
 	public void fullBundleAdjustment(int iterations) {
+
+		SceneStructureMetric scene = new SceneStructureMetric(false);
+		SceneObservations observations = new SceneObservations();
+
+		// load data into these neat lists
 		List<Pose> cameras = new ArrayList<Pose>();
 		List<Point3D> point3Ds = new ArrayList<Point3D>();
 		List<List<Observation>> obsv = new ArrayList<List<Observation>>();
 
 		// lock the map and load data
 		synchronized (this.map) {
-			// load camera list
+
+			//////////////////////
+			// load camera list //
+			//////////////////////
 			for (int i = 0; i < this.map.getKeyframes().size(); i++) {
 				cameras.add(this.map.getKeyframes().get(i).getPose());
 			}
@@ -52,9 +69,9 @@ public class MapOptimizer {
 				point3Ds.add(point);
 
 				// initialize observation list
-				List<Observation> observations = new ArrayList<Observation>();
+				List<Observation> observationsList = new ArrayList<Observation>();
 				for (int j = 0; j < cameras.size(); j++) {
-					observations.add(null);
+					observationsList.add(null);
 				}
 
 				// for each observation on this point, find its corresponding keyframe
@@ -64,18 +81,64 @@ public class MapOptimizer {
 					for (int k = 0; k < cameras.size() && index == -1; k++) {
 						index = cameras.get(k) == o.getKeyframe().getPose() ? k : -1;
 					}
-					observations.set(index, o);
+					observationsList.set(index, o);
 				}
 
 				// add observation list to obsv
-				obsv.add(observations);
+				obsv.add(observationsList);
 
+			}
+
+			///////////////////////////
+			// load lists into scene //
+			///////////////////////////
+			scene.initialize(cameras.size(), cameras.size(), point3Ds.size());
+			observations.initialize(cameras.size());
+
+			// load camera poses into scene
+			BundlePinhole camera = new BundlePinhole();
+			camera.fx = CameraIntrinsics.fx;
+			camera.fy = CameraIntrinsics.fy;
+			camera.cx = CameraIntrinsics.cx;
+			camera.cy = CameraIntrinsics.cy;
+			camera.skew = CameraIntrinsics.s;
+			for (int i = 0; i < cameras.size(); i++) {
+				Se3_F64 worldToCameraGL = new Se3_F64();
+				ConvertRotation3D_F64.quaternionToMatrix(cameras.get(i).getQw(), cameras.get(i).getQx(),
+						cameras.get(i).getQy(), cameras.get(i).getQz(), worldToCameraGL.R);
+				worldToCameraGL.T.x = cameras.get(i).getTx();
+				worldToCameraGL.T.y = cameras.get(i).getTy();
+				worldToCameraGL.T.z = cameras.get(i).getTz();
+				scene.setCamera(i, true, camera);
+				scene.setView(i, cameras.get(i).isFixed(), worldToCameraGL);
+				scene.connectViewToCamera(i, i);
+			}
+
+			// load projected observations into observations variable
+			for (int pointID = 0; pointID < obsv.size(); pointID++) {
+				for (int cameraID = 0; cameraID < obsv.get(pointID).size(); cameraID++) {
+					if (obsv.get(pointID).get(cameraID) == null) {
+						continue;
+					}
+					float pixelX = (float) obsv.get(pointID).get(cameraID).getPoint().x;
+					float pixelY = (float) obsv.get(pointID).get(cameraID).getPoint().y;
+					observations.getView(cameraID).add(pointID, pixelX, pixelY);
+				}
+			}
+
+			// load 3D points into scene
+			for (int i = 0; i < point3Ds.size(); i++) {
+				float x = (float) point3Ds.get(i).getX();
+				float y = (float) point3Ds.get(i).getY();
+				float z = (float) point3Ds.get(i).getZ();
+
+				scene.setPoint(i, x, y, z);
 			}
 
 		}
 
 		// perform BA
-		SceneStructureMetric scene = BundleAdjustor.bundleAdjust(cameras, point3Ds, obsv, iterations);
+		this.bundleAdjustScene(scene, observations, iterations);
 
 		// lock the map and update the points
 		synchronized (this.map) {
@@ -174,6 +237,56 @@ public class MapOptimizer {
 				cameras.get(viewID).setT(t.x, t.y, t.z);
 			}
 		}
+	}
+
+	public void bundleAdjustScene(SceneStructureMetric scene, SceneObservations observations, int maxIterations) {
+
+		ConfigLevenbergMarquardt configLM = new ConfigLevenbergMarquardt();
+		configLM.dampeningInitial = 1e-3;
+		configLM.hessianScaling = true;
+
+		ConfigBundleAdjustment configSBA = new ConfigBundleAdjustment();
+		configSBA.configOptimizer = configLM;
+		BundleAdjustment<SceneStructureMetric> bundleAdjustment = FactoryMultiView.bundleSparseMetric(configSBA);
+
+		// debug
+		bundleAdjustment.setVerbose(System.out, 0);
+
+		// Specifies convergence criteria
+		bundleAdjustment.configure(1e-12, 1e-12, maxIterations);
+
+		// Scaling each variable type so that it takes on a similar numerical
+		// value. This aids in optimization
+		// Not important for this problem but is for others
+		ScaleSceneStructure bundleScale = new ScaleSceneStructure();
+		bundleScale.applyScale(scene, observations);
+		bundleAdjustment.setParameters(scene, observations);
+
+		// Runs the solver. This will take a few minutes. 7 iterations takes
+		// about 3 minutes on my computer
+		long startTime = System.currentTimeMillis();
+		double errorBefore = bundleAdjustment.getFitScore();
+		Utils.pl("error before: " + errorBefore);
+		if (!bundleAdjustment.optimize(scene)) {
+			// throw new RuntimeException("Bundle adjustment failed?!?");
+			Utils.pl("\n\n\n\n***************************  ERROR  ****************************");
+			Utils.pl("NOTE: Bundle Adjustment failed!");
+			Utils.pl("fit score: " + bundleAdjustment.getFitScore());
+			bundleScale.undoScale(scene, observations);
+
+			Utils.pl("****************************************************************\n\n\n\n");
+
+		}
+
+		// Print out how much it improved the model
+		System.out.println();
+		System.out.printf("Error reduced by %.1f%%\n", (100.0 * (errorBefore / bundleAdjustment.getFitScore() - 1.0)));
+		System.out.println((System.currentTimeMillis() - startTime) / 1000.0);
+
+		// Return parameters to their original scaling. Can probably skip this
+		// step.
+		bundleScale.undoScale(scene, observations);
+
 	}
 
 	public Map getMap() {
--- src/main/java/ARSystem04/TestPipeline.java
+++ src/main/java/ARSystem04/TestPipeline.java
@@ -10,6 +10,7 @@ import Jama.Matrix;
 import buffers.Buffer;
 import buffers.QueuedBuffer;
 import buffers.SingletonBuffer;
+import runtimevars.CameraIntrinsics;
 import toolbox.Utils;
 import types.Correspondence2D2D;
 import types.FramePack;
@@ -117,22 +118,11 @@ public class TestPipeline extends PoseEstimator {
 						pose);
 
 				// if poses are far enough away, triangulate untriangulated points
-//				Utils.pl("pose.getDistanceFrom(this.map.getCurrentKeyframe().getPose()): "
-//						+ pose.getDistanceFrom(this.map.getCurrentKeyframe().getPose()));
-//				Utils.pl("number of untriangulated points: " + untriangulatedMapPoints.size());
-				if (pose.getDistanceFrom(this.map.getCurrentKeyframe().getPose()) >= 1) {
-					for (int i = 0; i < untriangulatedCorrespondences.size(); i++) {
-						Correspondence2D2D c = untriangulatedCorrespondences.get(i);
-						Matrix pointMatrix = Photogrammetry.triangulate(pose.getHomogeneousMatrix(),
-								this.map.getCurrentKeyframe().getPose().getHomogeneousMatrix(), c);
-						Point3D point3D = new Point3D(pointMatrix.get(0, 0), pointMatrix.get(1, 0),
-								pointMatrix.get(2, 0));
-						untriangulatedMapPoints.get(i).setPoint(point3D);
-						synchronized (this.map) {
-							this.map.registerPoint(point3D);
-						}
+				if (pose.getDistanceFrom(this.map.getCurrentKeyframe().getPose()) >= 1
+						&& untriangulatedCorrespondences.size() > 0) {
 
-					}
+					this.triangulateUntrackedMapPoints(pose, untriangulatedCorrespondences, untriangulatedMapPoints);
+
 				}
 
 				// if starting to lose tracking, generate new keyframe
@@ -179,5 +169,99 @@ public class TestPipeline extends PoseEstimator {
 			}
 
 		}
+	}
+
+	public void triangulateUntrackedMapPoints(Pose currentPose, List<Correspondence2D2D> untriangulatedCorrespondences,
+			List<MapPoint> untriangulatedMapPoints) {
+
+		// the sum of average reprojection errors over which the triangulation will be
+		// discarded
+		double AVG_THRESHOLD = 20;
+
+		// get point triangulations
+		List<Point3D> newPoints = new ArrayList<Point3D>();
+		for (int i = 0; i < untriangulatedCorrespondences.size(); i++) {
+			Correspondence2D2D c = untriangulatedCorrespondences.get(i);
+			Matrix pointMatrix = Photogrammetry.triangulate(currentPose.getHomogeneousMatrix(),
+					this.map.getCurrentKeyframe().getPose().getHomogeneousMatrix(), c);
+			Point3D point3D = new Point3D(pointMatrix.get(0, 0), pointMatrix.get(1, 0), pointMatrix.get(2, 0));
+			newPoints.add(point3D);
+		}
+
+		// evaluate error
+		double avgErrCurrent = 0;
+		double avgErrKeyframe = 0;
+		List<Double> currentErrors = new ArrayList<Double>();
+		List<Double> keyframeErrors = new ArrayList<Double>();
+		for (int i = 0; i < newPoints.size(); i++) {
+			Matrix point = new Matrix(4, 1);
+			point.set(0, 0, newPoints.get(i).getX());
+			point.set(1, 0, newPoints.get(i).getY());
+			point.set(2, 0, newPoints.get(i).getZ());
+			point.set(3, 0, 1);
+			Matrix projCurrent = CameraIntrinsics.getK4x4().times(currentPose.getHomogeneousMatrix()).times(point);
+			projCurrent = projCurrent.times(1 / projCurrent.get(2, 0));
+			Matrix projKeyframe = CameraIntrinsics.getK4x4()
+					.times(this.map.getCurrentKeyframe().getPose().getHomogeneousMatrix()).times(point);
+			projKeyframe = projKeyframe.times(1 / projKeyframe.get(2, 0));
+
+			double errCurrent = Math
+					.sqrt(Math.pow(projCurrent.get(0, 0) - untriangulatedCorrespondences.get(i).getX1(), 2)
+							+ Math.pow(projCurrent.get(1, 0) - untriangulatedCorrespondences.get(i).getY1(), 2));
+			double errKeyframe = Math
+					.sqrt(Math.pow(projKeyframe.get(0, 0) - untriangulatedCorrespondences.get(i).getX0(), 2)
+							+ Math.pow(projKeyframe.get(1, 0) - untriangulatedCorrespondences.get(i).getY0(), 2));
+
+			avgErrCurrent += errCurrent;
+			avgErrKeyframe += errKeyframe;
+
+			currentErrors.add(errCurrent);
+			keyframeErrors.add(errKeyframe);
+
+		}
+
+		avgErrCurrent /= newPoints.size();
+		avgErrKeyframe /= newPoints.size();
+
+		// get standard deviation of each error
+		double stdDevCurrent = 0;
+		double stdDevKeyframe = 0;
+
+		for (int i = 0; i < currentErrors.size(); i++) {
+			stdDevCurrent += Math.pow(currentErrors.get(i) - avgErrCurrent, 2);
+			stdDevKeyframe += Math.pow(keyframeErrors.get(i) - avgErrKeyframe, 2);
+		}
+
+		stdDevCurrent /= currentErrors.size();
+		stdDevKeyframe /= keyframeErrors.size();
+		stdDevCurrent = Math.sqrt(stdDevCurrent);
+		stdDevKeyframe = Math.sqrt(stdDevKeyframe);
+
+		Utils.pl("Average current frame reprojection error: " + avgErrCurrent);
+		Utils.pl("Average keyframe frame reprojection error: " + avgErrKeyframe);
+
+		// if error is too high, reject the reconstruction
+		if (avgErrCurrent + avgErrKeyframe > AVG_THRESHOLD) {
+			return;
+		}
+
+		// set each point in the map
+		for (int i = 0; i < newPoints.size(); i++) {
+
+			// do not triangulate outliers (points with error above 1 standard deviation of
+			// the mean)
+			if (currentErrors.get(i) > stdDevCurrent + avgErrCurrent
+					|| keyframeErrors.get(i) > stdDevKeyframe + avgErrKeyframe) {
+				continue;
+			}
+
+			// set the point
+			Point3D point3D = newPoints.get(i);
+			synchronized (this.map) {
+				untriangulatedMapPoints.get(i).setPoint(point3D);
+				this.map.registerPoint(point3D);
+			}
+		}
+
 	}
 }
